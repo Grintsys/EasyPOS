395f378528fa4793ce3b4dfa385578bc77237e71
brgp <brgp@live.com>
2021-03-07 19:45:20 -0600

    inactivity timeout

--- EasyPOS/aspnet-core/src/Grintsys.EasyPOS.HttpApi.Host/Startup.cs
+++ EasyPOS/aspnet-core/src/Grintsys.EasyPOS.HttpApi.Host/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace Grintsys.EasyPOS
 {
@@ -10,6 +11,16 @@ namespace Grintsys.EasyPOS
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication<EasyPOSHttpApiHostModule>();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                options.Cookie.Name = "EasyPOS";
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
