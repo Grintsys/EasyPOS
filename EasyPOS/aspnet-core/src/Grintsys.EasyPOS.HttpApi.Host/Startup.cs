6a8dbdbfffd6f38d3afe28e3b7bc8b047184653a
brgp <brgp@live.com>
2021-03-09 20:24:13 -0600

    Reading values from appSettings

--- EasyPOS/aspnet-core/src/Grintsys.EasyPOS.HttpApi.Host/Startup.cs
+++ EasyPOS/aspnet-core/src/Grintsys.EasyPOS.HttpApi.Host/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
@@ -8,18 +9,26 @@ namespace Grintsys.EasyPOS
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication<EasyPOSHttpApiHostModule>();
 
             services.ConfigureApplicationCookie(options =>
             {
-                options.LoginPath = "/Account/Login";
-                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.LoginPath = Configuration["CookiesSettings:LoginPath"];
+                options.AccessDeniedPath = Configuration["CookiesSettings:AccessDeniedPath"];
                 options.SlidingExpiration = true;
                 options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-                options.Cookie.Name = "EasyPOS";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(Convert.ToInt32(Configuration["CookiesSettings:SessionTimeOutTime"]));
+                options.Cookie.Name = Configuration["CookiesSettings:CookiesName"];
             });
         }
 
