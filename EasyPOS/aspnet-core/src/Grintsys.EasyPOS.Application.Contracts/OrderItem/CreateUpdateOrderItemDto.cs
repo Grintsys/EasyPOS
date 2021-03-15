f53401a5f79f7955218006d8ab810f33f1bdf44d
brgp <brgp@live.com>
2021-03-14 21:35:31 -0600

    Minor change

--- EasyPOS/aspnet-core/src/Grintsys.EasyPOS.Application.Contracts/OrderItem/CreateUpdateOrderItemDto.cs
+++ EasyPOS/aspnet-core/src/Grintsys.EasyPOS.Application.Contracts/OrderItem/CreateUpdateOrderItemDto.cs
@@ -5,21 +5,14 @@ namespace Grintsys.EasyPOS.OrderItem
 {
     public class CreateUpdateOrderItemDto
     {
-        [Required] 
         public Guid OrderId { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Description { get; set; }
-        [Required]
         public string Code { get; set; }
-        [Required]
         public float SalePrice { get; set; }
-        [Required]
         public float Taxes { get; set; }
-        [Required]
         public int Quantity { get; set; }
-        [Required]
+        public int Discount { get; set; }
         public float TotalItem { get; set; }
     }
 }
