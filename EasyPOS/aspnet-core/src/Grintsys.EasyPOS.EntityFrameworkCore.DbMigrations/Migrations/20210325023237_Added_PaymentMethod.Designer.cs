00f422eb3b7f597474894c4e7ac008be349699ab
brgp <brgp@live.com>
2021-03-24 20:37:29 -0600

    Added migration

--- EasyPOS/aspnet-core/src/Grintsys.EasyPOS.EntityFrameworkCore.DbMigrations/Migrations/20210325023237_Added_PaymentMethod.Designer.cs
+++ EasyPOS/aspnet-core/src/Grintsys.EasyPOS.EntityFrameworkCore.DbMigrations/Migrations/20210325023237_Added_PaymentMethod.Designer.cs
@@ -4,15 +4,17 @@ using Grintsys.EasyPOS.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace Grintsys.EasyPOS.Migrations
 {
     [DbContext(typeof(EasyPOSMigrationsDbContext))]
-    partial class EasyPOSMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210325023237_Added_PaymentMethod")]
+    partial class Added_PaymentMethod
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
