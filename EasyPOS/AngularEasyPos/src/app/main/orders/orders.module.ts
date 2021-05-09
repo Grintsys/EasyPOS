2d39d8eb8f60ee60a76586fc8175be6935602d14
brgp <brgp@live.com>
2021-05-08 18:56:46 -0600

    renaming component

--- EasyPOS/AngularEasyPos/src/app/main/orders/orders.module.ts
+++ EasyPOS/AngularEasyPos/src/app/main/orders/orders.module.ts
@@ -20,7 +20,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { OrderService } from './order.service';
 import { OrderDetailsComponent } from './order/order-details/order-details.component';
-import { CreditNotesComponent } from './order/credit-and-debit-notes/credit-and-debit-notes.component';
+import { CreditNotesComponent } from './order/credit-notes/credit-notes.component';
 import { OrderComponent } from './order/order.component';
 import { OrderProductsComponent } from './order/order-products/order-products.component';
 import { SearchModule } from 'app/layout/components/search/search.module';
