4c431722cb5ef3c57f88ec4541b803e484813ec4
brgp <brgp@live.com>
2021-06-23 20:13:39 -0600

    Order Item changes

--- EasyPOS/aspnet-core/src/Grintsys.EasyPOS.Domain/Order/Order.cs
+++ EasyPOS/aspnet-core/src/Grintsys.EasyPOS.Domain/Order/Order.cs
@@ -11,7 +11,7 @@ namespace Grintsys.EasyPOS.Order
         public OrderType OrderType { get; set; }
         public List<CreditNote.CreditNote> CreditNotes { get; set; } = new List<CreditNote.CreditNote>();
         [CanBeNull] public PaymentMethod.PaymentMethod PaymentMethod { get; set; }
-        public override float ISV => Items.Sum(x => x.Taxes * x.SalePrice * x.Quantity);
+        public override float ISV => Items.Sum(x => x.TaxAmount * x.SalePrice * x.Quantity);
         public override float Discount => Items.Sum(x => (x.Discount /100) * x.SalePrice * x.Quantity);
         public override float SubTotal => Items.Sum(x => x.SalePrice * x.Quantity);
         public override float Total => Items.Sum(x => x.TotalItem);
